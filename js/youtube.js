902becd1512389d3b11b7dc4b9b63b1a478e3b5c
wknowpark797 <wknowpark797@gmail.com>
2023-06-07 12:19:48 +0900

    youtube 서브 페이지 로직 함수 분리

--- js/youtube.js
+++ js/youtube.js
@@ -32,11 +32,7 @@
 
 const wrap = document.querySelector('.youtube .wrap');
 
-const key = 'AIzaSyDwb_57BfoNHLxlZ-Mwn2O3VNVt2tFNNMw';
-const list = 'PLEJLcTMBRARfXocKJpD9DCEBS1kUxA7jl';
-const num = 10;
-const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${list}&key=${key}&maxResults=${num}`;
-
+/*
 fetch(url)
 	.then((res) => {
 		return res.json();
@@ -60,7 +56,9 @@ fetch(url)
           </div>
           
           <div class="pic">
-            <img src="${item.snippet.thumbnails.standard.url}" />
+            <img src="${item.snippet.thumbnails.standard.url}" alt=${
+				item.snippet.resourceId.videoId
+			} />
           </div>
         </article>
       `;
@@ -68,23 +66,75 @@ fetch(url)
 
 		wrap.innerHTML = tags;
 
-		/*
-      // then 구문 안쪽에서 동기적으로 DOM 요소가 동적으로 생성된 이후에만 해당 요소에 접근 가능
-      // 이벤트 위임으로 then 구문 밖에서 사용할 수 있다.
-      const pic = wrap.querySelectorAll('pic')[0];
-      pic.addEventListener('click', () => {
-        console.log('clicked');
-      });
-    */
+    // then 구문 안쪽에서 동기적으로 DOM 요소가 동적으로 생성된 이후에만 해당 요소에 접근 가능
+    // 이벤트 위임으로 then 구문 밖에서 사용할 수 있다.
+    const pic = wrap.querySelectorAll('pic')[0];
+    pic.addEventListener('click', () => {
+      console.log('clicked');
+    });
 	});
+*/
+
+fetchData();
 
 // 이벤트 위임
 wrap.addEventListener('click', (e) => {
 	if (e.target.nodeName !== 'IMG') return;
-	// console.log('img clicked');
-	console.log(e.currentTarget);
-	console.log(e.target);
+	console.log(e.target.getAttribute('alt'));
 });
+
+// 데이터 fetching 함수
+async function fetchData() {
+	const key = 'AIzaSyDwb_57BfoNHLxlZ-Mwn2O3VNVt2tFNNMw';
+	const list = 'PLEJLcTMBRARfXocKJpD9DCEBS1kUxA7jl';
+	const num = 10;
+	const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${list}&key=${key}&maxResults=${num}`;
+
+	const res = await fetch(url);
+	const data = await res.json();
+	console.log(data.items);
+
+	createList(data.items);
+}
+
+// 동적으로 목록을 생성하는 함수
+function createList(arr) {
+	let tags = '';
+
+	arr.forEach((item) => {
+		let tit = item.snippet.title;
+		let desc = item.snippet.description;
+		let date = item.snippet.publishedAt;
+
+		tags += `
+      <article>
+        <h2>${tit.length > 50 ? tit.substr(0, 50) + '...' : tit}</h2>
+
+        <div class="txt">
+          <p>${desc.length > 200 ? desc.substr(0, 200) + '...' : desc}</p>
+          <span>${date.split('T')[0].split('-').join('.')}</span>
+        </div>
+        
+        <div class="pic">
+          <img src="${item.snippet.thumbnails.standard.url}" alt=${
+			item.snippet.resourceId.videoId
+		} />
+        </div>
+      </article>
+    `;
+	});
+
+	wrap.innerHTML = tags;
+
+	/*
+    // then 구문 안쪽에서 동기적으로 DOM 요소가 동적으로 생성된 이후에만 해당 요소에 접근 가능
+    // 이벤트 위임으로 then 구문 밖에서 사용할 수 있다.
+    const pic = wrap.querySelectorAll('pic')[0];
+    pic.addEventListener('click', () => {
+      console.log('clicked');
+    });
+  */
+}
 
 /*
   // 가상 DOM이 만들어지는 것은 비동기 상태이므로 요소를 찾을 수 없다.
