0b6f86d0e2165b3caefbd43bd2f111de68b96bfb
wknowpark797 <wknowpark797@gmail.com>
2023-06-07 11:48:53 +0900

    이벤트 위임 개념 추가

--- js/youtube.js
+++ js/youtube.js
@@ -15,6 +15,21 @@
     - 배열값을 구분자로 이어붙여 하나의 문자열로 반환
 */
 
+/*
+  [ 이벤트 위임 ]
+  - Event Delegate
+  - 현재 없는 요소에 이벤트를 전달하기 위해 항상 있는 상위 부모요소에 이벤트를 위임
+    (이벤트 버블링을 활용한 개념)
+*/
+
+/*
+  [ e.target vs e.currentTarget ]
+  - e.target
+    - 현재 이벤트 구문상에 선택자로 연결되어 있는 요소를 지칭
+  - e.currentTarget
+    - 화면상에서 이벤트가 발생한 대상을 지칭
+*/
+
 const wrap = document.querySelector('.youtube .wrap');
 
 const key = 'AIzaSyDwb_57BfoNHLxlZ-Mwn2O3VNVt2tFNNMw';
@@ -52,4 +67,29 @@ fetch(url)
 		});
 
 		wrap.innerHTML = tags;
+
+		/*
+      // then 구문 안쪽에서 동기적으로 DOM 요소가 동적으로 생성된 이후에만 해당 요소에 접근 가능
+      // 이벤트 위임으로 then 구문 밖에서 사용할 수 있다.
+      const pic = wrap.querySelectorAll('pic')[0];
+      pic.addEventListener('click', () => {
+        console.log('clicked');
+      });
+    */
 	});
+
+// 이벤트 위임
+wrap.addEventListener('click', (e) => {
+	if (e.target.nodeName !== 'IMG') return;
+	// console.log('img clicked');
+	console.log(e.currentTarget);
+	console.log(e.target);
+});
+
+/*
+  // 가상 DOM이 만들어지는 것은 비동기 상태이므로 요소를 찾을 수 없다.
+  const pic = wrap.querySelectorAll('pic')[0];
+  pic.addEventListener('click', () => {
+    console.log('clicked');
+  });
+*/
