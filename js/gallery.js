81c85a7cdd9f399b1971d1eb2fa5da66e5f4183d
wknowpark797 <wknowpark797@gmail.com>
2023-06-08 11:44:37 +0900

    gallery 서브 페이지 이미지 렌더링

--- js/gallery.js
+++ js/gallery.js
@@ -1,7 +1,9 @@
 const wrap = document.querySelector('.gallery .wrap');
 const api_key = '7f259a4112d06fbef0736c84af20f014';
 const method_interest = 'flickr.interestingness.getList';
-const num = 10;
+const num = 5;
+
+// nojsoncallback: 객체가 아닌 함수 형태일 때
 const baseURL = `https://www.flickr.com/services/rest/?method=${method_interest}&api_key=${api_key}&format=json&nojsoncallback=1&per_page=${num}`;
 
 fetch(baseURL)
@@ -9,5 +11,20 @@ fetch(baseURL)
 		return res.json();
 	})
 	.then((data) => {
-		console.log(data);
+		console.log(data.photos.photo);
+
+		const items = data.photos.photo;
+		let tags = '';
+
+		items.forEach((item) => {
+			tags += `
+        <article>
+          <div class="pic">
+            <img src="https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg" />
+          </div>
+        </article>
+      `;
+		});
+
+		wrap.innerHTML = tags;
 	});
