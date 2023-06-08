b2b663dfb34dd004f2d95844cf3e6b9201d74e5f
unknown <wknowpark797@gmail.com>
2023-06-08 15:48:19 +0900

    gallery.js 주석 수정

--- js/gallery.js
+++ js/gallery.js
@@ -1,5 +1,5 @@
 const wrap = document.querySelector('.gallery .wrap');
-const loading = document.querySelector('.loading');
+const loading = document.querySelector('.gallery .loading');
 
 const api_key = '7f259a4112d06fbef0736c84af20f014';
 const method_interest = 'flickr.interestingness.getList';
@@ -37,8 +37,10 @@ fetch(baseURL)
 
 		/*
       [ isoLayout 처음 적용 시 이미지 카드가 겹치는 원인 ]
-      img DOM은 생성 되었지만 해당 DOM에 수반되는 소스 이미지가 아직 렌더링 되지 않은 시점에서 isoLayout구문이 호출되었기 때문
-      -> 해결방법: 동적으로 만들어진 모든 img DOM을 반복 하면서 onload 이벤트를 연결하여 모든 소스 이미지가 렌더링 완료가 된 시점에 isoLayout 함수를 호출
+      - img DOM은 생성 되었지만 해당 DOM의 소스 이미지가 아직 렌더링 되지 않은 시점에서 isoLayout구문이 호출되었기 때문
+				- 이미지의 높이값이 제대로 적용되지 않아 겹침 발생
+				- 해결방법: 동적으로 만들어진 모든 img DOM을 반복 하면서 
+					onload 이벤트를 연결하여 모든 소스 이미지가 렌더링 완료가 된 시점에 isoLayout 함수를 호출
     */
 
 		const imgArr = wrap.querySelectorAll('img');
@@ -46,7 +48,7 @@ fetch(baseURL)
 
 		for (const el of imgArr) {
 			el.onload = () => {
-				count++;
+				count++; // 이미지 태그의 소스가 렌더링 완료될 때마다 증가
 				console.log(count);
 
 				count === imgArr.length && isoLayout();
