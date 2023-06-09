5e89f78bb185472f5c307547dd16d1e13eca0162
wknowpark797 <wknowpark797@gmail.com>
2023-06-09 10:59:29 +0900

    gallery.js 함수 분리

--- js/gallery.js
+++ js/gallery.js
@@ -1,7 +1,7 @@
 const wrap = document.querySelector('.gallery .wrap');
 const loading = document.querySelector('.gallery .loading');
 const api_key = '7f259a4112d06fbef0736c84af20f014';
-const num = 500;
+const num = 50;
 const myId = '198471371@N05';
 
 // nojsoncallback: 객체가 아닌 함수 형태일 때
@@ -12,24 +12,28 @@ const method_user = 'flickr.people.getPhotos';
 const interestURL = `${baseURL}${method_interest}`;
 const userURL = `${baseURL}${method_user}&user_id=${myId}`;
 
-fetch(interestURL)
-	.then((res) => {
-		return res.json();
-	})
-	.then((data) => {
-		console.log(data.photos.photo);
+fetchData(interestURL);
 
-		const items = data.photos.photo;
-		let tags = '';
+async function fetchData(url) {
+	const res = await fetch(url);
+	const json = await res.json();
+	const items = json.photos.photo;
+	console.log(items);
 
-		items.forEach((item) => {
-			tags += `
+	createList(items);
+}
+
+function createList(arr) {
+	let tags = '';
+
+	arr.forEach((item) => {
+		tags += `
         <li class="item">
           <div>
             <a href="https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_b.jpg">
               <img class="thumb" src="https://live.staticflickr.com/${item.server}/${item.id}_${
-				item.secret
-			}_m.jpg" />
+			item.secret
+		}_m.jpg" />
             </a>
             <p>${item.title === '' ? 'Have a good day ~' : item.title}</p>
 
@@ -40,11 +44,15 @@ fetch(interestURL)
           </div>
         </li>
       `;
-		});
+	});
 
-		wrap.innerHTML = tags;
+	wrap.innerHTML = tags;
 
-		/*
+	setLoading();
+}
+
+function setLoading() {
+	/*
       [ isoLayout 처음 적용 시 이미지 카드가 겹치는 원인 ]
       - img DOM은 생성 되었지만 해당 DOM의 소스 이미지가 아직 렌더링 되지 않은 시점에서 isoLayout구문이 호출되었기 때문
 				- 이미지의 높이값이 제대로 적용되지 않아 겹침 발생
@@ -52,24 +60,24 @@ fetch(interestURL)
 					onload 이벤트를 연결하여 모든 소스 이미지가 렌더링 완료가 된 시점에 isoLayout 함수를 호출
     */
 
-		const imgArr = wrap.querySelectorAll('img');
-		let count = 0;
+	const imgArr = wrap.querySelectorAll('img');
+	let count = 0;
 
-		for (const el of imgArr) {
-			// 이미지의 엑박 오류 발생 시 onerror 이벤트로 잡아 디폴트 이미지로 대체
-			el.onerror = () => {
-				el.setAttribute('src', 'https://www.flickr.com/images/buddyicon.gif');
-			};
+	for (const el of imgArr) {
+		// 이미지의 엑박 오류 발생 시 onerror 이벤트로 잡아 디폴트 이미지로 대체
+		el.onerror = () => {
+			el.setAttribute('src', 'https://www.flickr.com/images/buddyicon.gif');
+		};
 
-			// 디폴트 이미지까지 포함하여 카운트 (무한로딩 오류 처리)
-			el.onload = () => {
-				count++; // 이미지 태그의 소스가 렌더링 완료될 때마다 증가
-				console.log(count);
+		// 디폴트 이미지까지 포함하여 카운트 (무한로딩 오류 처리)
+		el.onload = () => {
+			count++; // 이미지 태그의 소스가 렌더링 완료될 때마다 증가
+			console.log(count);
 
-				count === imgArr.length && isoLayout();
-			};
-		}
-	});
+			count === imgArr.length && isoLayout();
+		};
+	}
+}
 
 function isoLayout() {
 	new Isotope(wrap, {
