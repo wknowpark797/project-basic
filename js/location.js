66c7e8b1b614ae77fea3a2930ec4633079a587b7
wknowpark797 <wknowpark797@gmail.com>
2023-06-16 12:20:46 +0900

    location 서브 페이지 마커 이미지 변경

--- js/location.js
+++ js/location.js
@@ -6,11 +6,23 @@
   3. 해당 테스트 화면에서 정밀하게 원하는 지점을 클릭하여 해당 코드값을 활용
 */
 
+// 기본 지도 생성
 const mapContainer = document.querySelector('#map');
 const position = new kakao.maps.LatLng(37.51269647949463, 127.06068851054262); // 지도 위치 인스턴스
 const mapOption = { center: position, level: 3 }; // 지도 생성 옵션
 const map = new kakao.maps.Map(mapContainer, mapOption); // 지도 인스턴스 생성
-const marker = new kakao.maps.Marker({ position: position }); // 마커 인스턴스 생성
 
-// 마커 인스턴스에서 setMap 함수로 지도 인스턴스 바인딩
+// Marker 이미지 변경
+const imageSrc = 'img/marker1.png';
+const imageSize = new kakao.maps.Size(232, 99);
+const imageOption = { offset: new kakao.maps.Point(116, 99) };
+const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
+
+// Marker 인스턴스 생성
+const marker = new kakao.maps.Marker({
+	position: position,
+	image: markerImage,
+});
+
+// Marker 인스턴스에서 setMap 함수로 지도 인스턴스 바인딩
 marker.setMap(map);
